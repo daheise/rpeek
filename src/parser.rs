e4713872ce94420c55312cb485702ff9552c7718
daheise <daheise@users.noreply.github.com>
2016-09-09 14:55:54 -0400

    Let Rust handle the buffering. also switch to using the built in path helpers.

--- src/parser.rs
+++ src/parser.rs
@@ -1,27 +1,43 @@
+use std::path::Path;
 use std::fs::File;
 
 struct SourceText {
     at_eof: bool,
-    block: usize,
-    blocks: [[char; 2]; 1024],
     eof_count: usize,
     name: String,
-    next: usize,
     column: usize,
     line: usize,
-    initialized: bool,
     stream: File,
 }
 
 trait Parser{
-    fn new(name: &str) -> Self;
-    fn consume(&self) -> Result<char, &'static str>;
-    fn consume_n(&self, n: usize) ->  Result<&str, &'static str>;
-    fn consume_whitespace(&self) -> Result<(),  &'static str>;
-    fn eof(&self) -> bool;
-    fn loc(&self) -> (usize, usize);
-    fn peek(&self) -> Result<char, &'static str>;
-    fn peek_n(&self, n: usize) -> Result<&str, &'static str>;
-    fn peek_str(&self, s_str: &str) -> Result<bool, &'static str>;
-    fn consume_str(&self, s_str: &str) -> Result<bool, &'static str>;
+    fn new(path: &Path) -> Self;
+    //fn consume(&self) -> Result<char, &'static str>;
+    //fn consume_n(&self, n: usize) ->  Result<&str, &'static str>;
+    //fn consume_whitespace(&self) -> Result<(),  &'static str>;
+    //fn eof(&self) -> bool;
+    //fn loc(&self) -> (usize, usize);
+    //fn peek(&self) -> Result<char, &'static str>;
+    //fn peek_n(&self, n: usize) -> Result<&str, &'static str>;
+    //fn peek_str(&self, s_str: &str) -> Result<bool, &'static str>;
+    //fn consume_str(&self, s_str: &str) -> Result<bool, &'static str>;
+}
+
+impl Parser for SourceText{
+    fn new(path: &Path) -> SourceText {
+        // TODO: Error checking
+        let f = File::open(path).unwrap();
+        // TODO: Lots of error checking
+        let filename = path.file_name().unwrap().to_str().unwrap().to_owned();
+        print!("{}", filename);
+
+        SourceText {
+            at_eof: false,
+            eof_count: 0,
+            name: filename,
+            column: 0,
+            line: 0,
+            stream: f
+        }
+    }
 }
